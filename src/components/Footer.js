0019b4debfe56e6fb1c982aafcd33295a6f5b68d
VookmirVookmir <mitchell.ma.robbins@gmail.com>
2021-12-05 12:11:01 -0600

    finalized footer, added better icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,32 @@
+import { ICONS } from "jest-util/build/specialChars";
 import React from "react";
-
+import { Row, Col, } from 'react-bootstrap';
+import Github from "./images/github-3.svg"
+import Linkdin from "./images/linkedin-3.svg"
+import Resume from "./images/cv-4.svg"
 
 function Footer(){
     return(
         <div>
 {/* footer here */}
-
-
+        <Row className='justify-content-center text-center' xs="auto">
+        
+        <Col >
+        <a href= "https://github.com/Mitchell-est-Robbins" >
+            <img src={Github} alt="Github icon and link"></img>
+        </a>
+        </Col>
+        <Col >
+        <a href= "https://www.linkedin.com/in/mrmitchellrobbins/" >
+            <img src={Linkdin} alt="Linkdin icon and link"></img>
+        </a>
+        </Col>
+        <Col >
+        <a href= "https://my.indeed.com/p/mitchellr-czg7i0c" >
+            <img src={Resume} alt="Resume icon and link"></img>
+        </a>
+        </Col>
+        </Row>
         </div>
 
 
